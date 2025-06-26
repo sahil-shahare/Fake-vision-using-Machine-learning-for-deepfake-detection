b27edbe9b1cc00980b925a5103ebe1761e95db41
sahil-shahare <sahilshahare380@gmail.com>
2025-06-26 12:45:28 +0530

    V1.0

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ if __name__ == '__main__':
             db.session.add(User(username="admin", email="admin@example.com", password=default_pw))
             db.session.commit()
 
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
